6959e6d3e2817ccd199f64df13b12adfb670e994
azijur434344 <141319988+azijur434344@users.noreply.github.com>
2023-08-24 16:24:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// const incomeInput = document.getElementById("income");
+// const expenseInputs = [document.getElementById("expense1"), document.getElementById("expense2"), document.getElementById("expense3")];
+// const calculateButton = document.getElementById("calculate");
+// const balanceDisplay = document.getElementById("balance");
+
+// calculateButton.addEventListener("click", calculateBalance);
+
+// function calculateBalance() {
+//     const income = parseFloat(incomeInput.value);
+//     const expenses = expenseInputs.reduce((total, input) => total + parseFloat(input.value || 0), 0);
+//     const saving = income * 0.2;
+//     const balance = income - expenses - saving;
+    
+//     balanceDisplay.textContent = balance.toFixed(2);
+// }
+
